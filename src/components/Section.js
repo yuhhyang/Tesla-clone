a3760d3e1ece750ab527cdfa1fde9a78a74e0fa5
Helen <yuhan.yangg@gmail.com>
2022-01-28 05:11:08 +0800

    react-reveal

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
 function Section({ title, description, backgroundImg, leftBtnText, rightBtnText }) {
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{title}</h1>
-                <p>{description}</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        {leftBtnText}
-                    </LeftButton>
-                    {/* 因為 Accessories 只有一個 button， leftbutton 無法置中顯示 */}
-                    { rightBtnText &&  
-                        <RightButton>
-                            {rightBtnText}
-                        </RightButton>
-                    }
-                    
-                </ButtonGroup>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
+                        {/* 因為 Accessories 只有一個 button， leftbutton 無法置中顯示 */}
+                        { rightBtnText &&  
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                        }
+                        
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src="./images/down-arrow.svg"/>
             </Buttons>
         </Wrap>
