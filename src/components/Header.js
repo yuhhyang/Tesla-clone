86486bb1bb53d72035fbda73fcd36d4d24e832c9
Helen <yuhan.yangg@gmail.com>
2022-01-28 05:47:57 +0800

    BurgerNav style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
+
 function Header() {
     return (
         <Container>
@@ -18,6 +19,22 @@ function Header() {
                 <a href='#'>Tesla Account</a>
                 <CustomMenu/>
             </RightMenu>
+            <BurgerNav>
+                <li><a href='#'>Existing Inventory</a></li>
+                <li><a href='#'>Used Inventory</a></li>
+                <li><a href='#'>Trade-In</a></li>
+                <li><a href='#'>Test Drive</a></li>
+                <li><a href='#'>Cybertruck</a></li>
+                <li><a href='#'>Roadster</a></li>
+                <li><a href='#'>Semi</a></li>
+                <li><a href='#'>Charging</a></li>
+                <li><a href='#'>Powerwall</a></li>
+                <li><a href='#'>Commercial Energy</a></li>
+                <li><a href='#'>Utilities</a></li>
+                <li><a href='#'>Find Us</a></li>
+                <li><a href='#'>Support</a></li>
+                <li><a href='#'>Investor Relations</a></li>
+            </BurgerNav>
         </Container>
     );
 }
@@ -34,6 +51,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1;/* Coverd by BurgerNav */
 `
 const Menu = styled.div`
     display: flex;
@@ -61,5 +79,32 @@ const RightMenu = styled.div`
 `
 const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
-
 `
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color: white;
+    width: 300px;
+    z-index: 10;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    li {
+        font-size: 14px;
+        padding: 15px 0;
+
+    }
+    li:hover {
+        background-color: #F0F0F0;
+        width: 180px;
+        height: 50px;
+        border-radius: 8px;
+    }
+    a {
+        font-weight: 600;
+    }
+` 
